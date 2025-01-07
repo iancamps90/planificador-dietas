674c802cf70f47231abcc7b802f250807f6ac542
iancamps90 <iancamps90@gmail.com>
2025-01-07 20:30:00 +0100

    Initial commit with Vite setup

---
+++ planificador-dietas/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  
+  const App = () => {
+    return (
+      <div>
+        <h1>Planificador de Dietas</h1>
+        <p>Bienvenido a tu herramienta personalizada para planificar dietas.</p>
+      </div>
+    );
+
+  }
+
+
+export default App;
